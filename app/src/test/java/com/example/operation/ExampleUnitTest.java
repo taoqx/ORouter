5e35392a3c3c98b89a81a3f340db6a4873187de9
ligen <ligen0227@qq.com>
2020-05-07 12:07:11 +0800

    删除冗余代码

--- app/src/test/java/com/example/operation/ExampleUnitTest.java
+++ app/src/test/java/com/example/operation/ExampleUnitTest.java
@@ -14,13 +14,6 @@ import static org.junit.Assert.assertEquals;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        ORouter.init();
-        ORouter.start("{\n" +
-                "\"path\":\"test\",\n" +
-                "\"message\":\"it is test message\"\n" +
-                "}");
-
-
         assertEquals(4, 2 + 2);
     }
 
